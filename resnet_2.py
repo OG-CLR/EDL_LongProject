bf2db5c8b281135501356a7bc37ea653c69218f4
Timothee LEVEILLE <t23levei@fl-tp-br-547.imta.fr>
2025-05-19 17:36:20 +0200

    resnet_2 et resnet_4

--- resnet_2.py
+++ resnet_2.py
@@ -10,7 +10,41 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class BasicBlockFactorized(nn.Module):
+    expansion = 1
 
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlockFactorized, self).__init__()
+
+        # Depthwise + pointwise for conv1
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_planes, in_planes, kernel_size=3, stride=stride, padding=1, groups=in_planes, bias=False),  # depthwise
+            nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)  # pointwise
+        )
+        self.bn1 = nn.BatchNorm2d(planes)
+
+        # Depthwise + pointwise for conv2
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, groups=planes, bias=False),
+            nn.Conv2d(planes, planes, kernel_size=1, bias=False)
+        )
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        # Shortcut branch
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+    
 class BasicBlock(nn.Module):
     expansion = 1
 
@@ -107,7 +141,7 @@ class ResNet(nn.Module):
 
 
 def ResNet18():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
+    return ResNet(BasicBlockFactorized, [2, 2, 2, 2])
 
 
 def ResNet34():
